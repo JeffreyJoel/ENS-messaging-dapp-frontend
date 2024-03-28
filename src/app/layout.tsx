10fe760e121e6c7f16bef0ac702e5d15f6dc01f9
JeffreyJoel <jowoloko@gmail.com>
2024-03-28 17:20:40 +0100

    update ens-messaging-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} dark`}>
         <Web3Modal>
         {children}
         </Web3Modal>
